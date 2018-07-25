44391789ac0e7058ac9e633e207db70a02d026ff
stopherjones <chrisjones59@googlemail.com>
2018-07-25 16:10:42 +0100

    Add js file

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/* toggle between hiding and showing the dropdown content */
+function myFunction2() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+/* Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+*/
